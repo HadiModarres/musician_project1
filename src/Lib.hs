abe1c2f71a9ad680822cbfdc092365be4e8a053e
Hadi <modarres.zadeh@gmail.com>
2019-04-10 12:15:18 +1000

    -

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,30 @@
 module Lib
-    ( someFunc
+    ( someFunc,
+    Pitch,
+    toPitch,
+    feedback,
+    GameState,
+    initialGuess,
+    nextGuess
     ) where
+
+toPitch :: String -> Maybe Pitch
+feedback ::[Pitch] -> [Pitch] -> (Int,Int,Int)
+initialGuess :: ([Pitch],GameState)
+nextGuess :: ([Pitch],GameState) -> (Int,Int,Int) -> ([Pitch],GameState)
+
+data Note = A | B
+  deriving Show
+data Octave = One | Two
+  deriving Show
+
+data Pitch = Pitch Note Octave
+
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+
+
+
